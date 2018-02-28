7031e94655abcb8cc548fdb500c7f8b39f370b82
Kevin Faber <kfaber3234@gmail.com>
2018-02-28 15:36:58 -0500

    Added a new struct

--- Assignment 1/Assignment 1/Main.cpp
+++ Assignment 1/Assignment 1/Main.cpp
@@ -1,4 +1,20 @@
-int main() {
+// this is a test file
+// lets test the conflicts
+#include <iostream>
+struct sendHelp {
 
+	void print() {
+		std::cout << "please\n";
+	}
+};
+
+int main() {
+	sendHelp s;
+
+	s.print();
+
+	getchar();
+
+	return 0;
 
 }
